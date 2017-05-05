d9165047c3f4f79f451dc8ea723b5158f7498bca
babyisun@qq.com <babyisun@qq.com>
2017-05-05 14:52:10 +0800

    child router

--- react-flux/src/Mazda/js/page/mazda.js
+++ react-flux/src/Mazda/js/page/mazda.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, HashRouter,Route, Link} from 'react-router-dom';
+import {BrowserRouter, HashRouter, Route, Link} from 'react-router-dom';
 // import {createBrowserHistory} from 'history';
 // const history = createBrowserHistory();
 import {createHashHistory} from 'history';
@@ -9,28 +9,29 @@ const history = createHashHistory();
 import Home from '../component/Home.jsx';
 import Login from '../component/Login.jsx';
 
-/*class Home extends React.Component {
+class Page extends React.Component {
     render() {
-        return (<div>home
+        return (<div>
+            <div>header</div>
+            {/*<div>{this.props.children}</div>*/}
+            <div>
+                <Route exact path="/index" component={Home}/>
+                <Route exact path="/login" component={Login}/>
+            </div>
+            <div>footer</div>
         </div>)
     }
 }
-class Login extends React.Component {
-    render() {
-        return (<div>Login
-        </div>)
-    }
-}*/
 
 
 ReactDOM.render(<HashRouter history={history}>
         <div>
             <ul>
-                <li><Link to="/">Home</Link></li>
+                <li><Link to="/index">Home</Link></li>
                 <li><Link to="/login">login</Link></li>
             </ul>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/login" component={Login}/>
+            <Route path="/" component={Page}/>
+
         </div>
     </HashRouter>,
     document.getElementById('page')

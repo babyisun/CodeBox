d55734f1a237ffc4297a6a9b0f2a6ad64482ed5c
babyisun@qq.com <babyisun@qq.com>
2017-05-08 17:36:01 +0800

    test router

--- react-flux/src/Mazda/js/component/Home.jsx
+++ react-flux/src/Mazda/js/component/Home.jsx
@@ -7,7 +7,7 @@ export default class Home extends React.Component {
    
     render() {
         return (<div>
-                Home
+                Homeqqqqq
             </div>
         )
     }
--- react-flux/src/Mazda/js/page/mazda.js
+++ react-flux/src/Mazda/js/page/mazda.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, HashRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, HashRouter, Route, Link,Redirect} from 'react-router-dom';
 // import {createBrowserHistory} from 'history';
 // const history = createBrowserHistory();
 import {createHashHistory} from 'history';
@@ -10,11 +10,17 @@ import Home from '../component/Home.jsx';
 import Login from '../component/Login.jsx';
 
 class Page extends React.Component {
+    constructor(props) {
+        super(props);
+        //console.log(props);
+    }
     render() {
+        console.log(this.props);
         return (<div>
             <div>header</div>
             {/*<div>{this.props.children}</div>*/}
             <div>
+                <Redirect to='/index'/>
                 <Route exact path="/index" component={Home}/>
                 <Route exact path="/login" component={Login}/>
             </div>

a204f5eef634bddc4789c19421831adad5d24938
North <babyisun@qq.com>
2017-06-18 19:42:55 +0800

    nothing to say

--- CBS_dev/js/index.js
+++ CBS_dev/js/index.js
@@ -1,26 +1,24 @@
 (function(window, ducument, $) {
 
-     //日历
-    var _calendar,$calendar;
-    $(".time_fil").click(function(){
-        var _this=$(this);
-        if($calendar){
+    //日历
+    var _calendar, $calendar;
+    $(".time_fil").click(function() {
+        var _this = $(this);
+        if ($calendar) {
             $calendar.show();
-        }
-        else{
-            var options = $.extend( {}, options,
-                {
-                    calendarID:"time_calendar",
-                    callback: function (_calendar) {
-                        var selectDate = _calendar.getSelectedDate(); //获取选中日期
-                        var startdate=dateExtend.format(new Date(selectDate[0]), 'MM.DD'),
-                        enddate=dateExtend.format(new Date(selectDate[1]), 'MM.DD');
-                        _this.find('#time_calendar_value').html(startdate+"-"+enddate);
-                    }
-                });
-        _calendar=new Calendar(options);
-        _calendar.setSelectedDate(dateExtend.format(new Date(),"YYYY-MM-DD") + "," + dateExtend.format(dateExtend.add(new Date(),1),"YYYY-MM-DD") );
-        $calendar=$("#time_calendar").show();
+        } else {
+            var options = $.extend({}, options, {
+                calendarID: "time_calendar",
+                callback: function(_calendar) {
+                    var selectDate = _calendar.getSelectedDate(); //获取选中日期
+                    var startdate = dateExtend.format(new Date(selectDate[0]), 'MM.DD'),
+                        enddate = dateExtend.format(new Date(selectDate[1]), 'MM.DD');
+                    _this.find('#time_calendar_value').html(startdate + "-" + enddate);
+                }
+            });
+            _calendar = new Calendar(options);
+            _calendar.setSelectedDate(dateExtend.format(new Date(), "YYYY-MM-DD") + "," + dateExtend.format(dateExtend.add(new Date(), 1), "YYYY-MM-DD"));
+            $calendar = $("#time_calendar").show();
         }
 
         _calendar.settings.isOpen = true;
@@ -28,11 +26,21 @@
         _calendar.settings.currScrollTop = $(window).scrollTop();
         //定位到开始日期高度
         var selectDate = _calendar.getSelectedDate(); //获取选中日期
-        var _startT = dateExtend.format(new Date(selectDate[0]),"YYYY-MM-01"),
+        var _startT = dateExtend.format(new Date(selectDate[0]), "YYYY-MM-01"),
             _startDom = $calendar.find('span[data-date="' + _startT + '"]'),
             _startPos = _startDom.offset();
-            console.log(_startPos);
+        console.log(_startPos);
         $(window).scrollTop(_startPos.top);
+    });
+
+    //登录菜单
+    $(".icon-list").click(function() {
+        console.log(1);
+        if ($(".logo-lis").css("display") == "none") {
+            $(".logo-lis").show();
+        } else {
+            $(".logo-lis").hide();
+        }
     });
 
     // 滑块
@@ -49,12 +57,10 @@
         " - ￥" + $("#slider-range").slider("values", 1));
 
 
-    // // 点击菜单
-    // $(".menue").click(function() {
-    //     $(this).toggleClass("active");
-    //     $(".navs").toggle("hide");
-    //     $(".filter").toggle("hide");
-    // });
+    // 点击菜单心
+    $(".icon-xiangqu").click(function() {
+        $(this).toggleClass("active");
+    });
 
     // 点击筛选
     $(".navs li").click(function() {
@@ -78,9 +84,13 @@
                     break;
                 case "area_fil":
                     $(".area").removeClass("hide").addClass("curr");
+                    var dHeight=$(document).height();
+                    $(".area").height(dHeight-174);
                     break;
                 case "more_fil":
                     $(".more").removeClass("hide").addClass("curr");
+                    var dHeight=$(document).height();
+                    $(".more").height(dHeight-134);
                     break;
                 case "reco_fil":
                     $(".recommend").removeClass("hide").addClass("curr");
@@ -88,36 +98,43 @@
             }
         }
     });
-    
+
     //点击客人数量
-    $(".custom li").click(function(){
+    $(".custom li").click(function() {
         $(".custom li").not(this).removeClass("active");
         $(this).toggleClass("active");
     })
 
     //点击房源类型
-    $(".type>div").click(function(){
+    $(".type>div").click(function() {
         $(".type div").not(this).removeClass("active");
         $(this).toggleClass("active")
     })
 
     //点击便利施舍
-    $(".facilities>div").click(function(){
+    $(".facilities>div").click(function() {
         $(".facilities div").not(this).removeClass("active");
         $(this).toggleClass("active")
     })
 
     //排序
-     $(".recommend .abled").click(function(){
+    $(".recommend .abled").click(function() {
         $(".recommend .abled").not(this).removeClass("active");
         $(this).toggleClass("active");
-        $(".reco_fil").removeClass("active").children('a').html($(this).html()+'<i class="trigelDown">');
+        $(".reco_fil").removeClass("active").children('a').html($(this).html() + '<i class="trigelDown">');
         $(".recommend").addClass("hide").removeClass("curr");
     })
 
-     //收藏
-     $(".favor").click(function(){
+    //收藏
+    $(".favor").click(function() {
         $(this).toggleClass("active");
-     })
+    })
+    
+            var mySwiper= new Swiper(".swiper-container",{
+            pagination:".swiper-pagination",
+            loop:true,
+            observer:true,
+            observeParents:true
+    });
 
 })(window, document, $);

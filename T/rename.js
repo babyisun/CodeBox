29f82d50cab06f721c3c30c9c77986d27acf20e3
North <babyisun@qq.com>
2017-10-25 08:13:09 +0800

    T

---
+++ T/rename.js
@@ -0,0 +1,47 @@
+//同以目录下的文件更名：
+var fs = require('fs');
+var data = require("./data.js");
+//console.log(data.math.code);
+
+
+var renameList = function (data, key, dir) {
+    if (data && data.data && data.data.list) {
+        var output = [];
+        for (var i = 0; i < data.data.list.length; i++) {
+            var temp = data.data.list[i];
+            if (temp && temp.title.indexOf(key) >= 0) {
+                for (var j = 0; j < temp.list.length; j++) {
+                    var t = temp.list[j];
+                    if (t) {
+                        var kv = {};
+                        if (t.title) {
+                            kv.t = key + t.title;
+                        }
+                        if (t && t.video) {
+                            var path = t.video.highWatchUrl.split("\/"),
+                                filename = path[path.length - 1];
+                            //console.log(path.length);
+                            kv.v = filename;
+                        }
+                        output.push(kv);
+                    }
+                }
+            }
+        }
+        if (output.length) {
+            for (var i = 0; i < output.length; i++) {
+                var temp = output[i];
+                fs.rename(dir + temp.v, dir + temp.t + ".mp4", function (err) {
+                    if (err) {
+                        console.log(temp);
+                        throw err;
+                    }
+                });
+            }
+        }
+        //console.log(output);
+    }
+}
+
+renameList(data.math, "2014年1月", "2014/");
+
